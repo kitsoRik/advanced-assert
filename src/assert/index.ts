2deccf6315a8ae13bc4207e19ce42e56750d0730
Rostyslav Pidburachynskyi <=>
2021-08-24 18:24:39 +0300

    Add error if overloaded assert function not found

--- src/assert/index.ts
+++ src/assert/index.ts
@@ -3,6 +3,12 @@ import { simpleAssert } from './simple-assert';
 export function assert<TCondition>(
   truthValue: TCondition,
   message: string,
+): asserts truthValue;
+export function assert<TCondition>(
+  truthValue: TCondition,
+  message: any,
 ): asserts truthValue {
   if (typeof message === 'string') return simpleAssert(truthValue, message);
+
+  throw new Error('Unexpected assert overloaded function');
 }
