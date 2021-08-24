fa6bb4c0454895e547cd25f619987fcc8b6cbca0
Rostyslav Pidburachynskyi <=>
2021-08-24 20:54:45 +0300

    Add test for assert directly

---
+++ __tests__/assert/index.test.ts
@@ -0,0 +1,53 @@
+import { AssertionError } from 'assert';
+import { assert } from '../../src';
+import { FALSY_VALUE, TRUTHY_VALUE } from '../helpers';
+
+describe('assert', () => {
+  describe('simple assert', () => {
+    it('should be passed', () => {
+      expect(() => assert(FALSY_VALUE, 'Value must be truthy')).toThrow(
+        AssertionError,
+      );
+    });
+
+    it('should be passed', () => {
+      expect(() => assert(TRUTHY_VALUE, 'Value must be truthy')).not.toThrow(
+        AssertionError,
+      );
+    });
+  });
+
+  describe('assert with error instance', () => {
+    class ErrorClass {}
+
+    it('should be passed', () => {
+      expect(() => assert(FALSY_VALUE, new ErrorClass())).toThrow(ErrorClass);
+    });
+
+    it('should be passed', () => {
+      try {
+        expect(() => assert(FALSY_VALUE, 1)).toThrow();
+      } catch (e) {
+        expect(e).toEqual(1);
+      }
+    });
+  });
+
+  describe('assert with error constructor', () => {
+    it('should be passed', () => {
+      class ErrorClass {}
+
+      expect(() => assert(FALSY_VALUE, ErrorClass)).toThrow(ErrorClass);
+    });
+
+    it('should be passed', () => {
+      class ErrorClass {
+        constructor(protected message: string, protected reason: string) {}
+      }
+
+      expect(() =>
+        assert(FALSY_VALUE, ErrorClass, 'message', 'reason'),
+      ).toThrow(ErrorClass);
+    });
+  });
+});
