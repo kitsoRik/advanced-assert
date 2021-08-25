efd98fd35a8b1297f8d66742ca093402b4fea3fd
Rostyslav Pidburachynskyi <=>
2021-08-25 19:57:33 +0300

    Add returning type to Settings.instance getter

--- src/settings.ts
+++ src/settings.ts
@@ -2,10 +2,10 @@ import { AdvancedAssertionError } from './error';
 import { ThrowAssertionErrorCallback } from './types';
 
 export class Settings {
-  private static _instance: Settings;
+  private static _instance: Settings = new Settings();
 
-  static get instance() {
-    return (Settings._instance = Settings._instance || new Settings());
+  static get instance(): Settings {
+    return Settings._instance;
   }
 
   protected _throwAssertionErrorCallback: ThrowAssertionErrorCallback = (
